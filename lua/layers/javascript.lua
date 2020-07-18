7e0d1d12fc4992e51f2625722a262dbe9b07d656
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-07-18 09:18:22 +0200

    feat(js): use flow instead of typescript-javascript-langserver

--- lua/layers/javascript.lua
+++ lua/layers/javascript.lua
@@ -7,7 +7,7 @@ function layer.init_config()
 	local lsp = require("layers.lsp")
 	local nvim_lsp = require("nvim_lsp")
 
-	lsp.register_server(nvim_lsp.javascript_typescript_langserver)
+	lsp.register_server(nvim_lsp.flow)
 end
 
 return layer
