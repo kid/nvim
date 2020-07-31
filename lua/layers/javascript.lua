d3af128f8dc52ee8352ae177a08710668bde241f
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-07-31 15:42:21 +0200

    js: uses javascript_typescript_langserver as LSP

--- lua/layers/javascript.lua
+++ lua/layers/javascript.lua
@@ -6,8 +6,24 @@ end
 function layer.init_config()
 	local lsp = require("layers.lsp")
 	local nvim_lsp = require("nvim_lsp")
+	local configs = require("nvim_lsp/configs")
+	local util = require("nvim_lsp/util")
 
-	lsp.register_server(nvim_lsp.flow)
+	local server_name = "javascript_typescript_langserver"
+	local bin_name = "javascript-typescript-stdio"
+
+	configs[server_name]  = {
+		default_config = {
+			cmd = { bin_name };
+			filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" };
+			root_dir = util.root_pattern("package.json", "tsconfig.json", ".git");
+			settings = {};
+		};
+	};
+
+	lsp.register_server(nvim_lsp.javascript_typescript_langserver)
+
+	-- lsp.register_server(nvim_lsp.flow)
 end
 
 return layer
