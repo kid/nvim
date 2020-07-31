c92e2bcf67ffbabe75726e6e3e28ece83c4dc6fc
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-07-31 16:58:08 +0200

    enable underline for diagnostics

--- lua/layers/lsp.lua
+++ lua/layers/lsp.lua
@@ -18,6 +18,7 @@ function layer.init_config()
 	lsp_status.register_progress()
 
 	vim.g.diagnostic_enable_virtual_text = 1
+	vim.g.diagnostic_enable_underline = 1
 	vim.g.completion_enable_auto_paren = 1
 	vim.g.completion_enable_snippet = 'vim-vsnip'
 
