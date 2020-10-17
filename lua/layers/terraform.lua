de67e6cb8cb0629aaa4326a9811a5832b15cd4cc
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-10-17 09:42:18 +0200

    feat: add terraform layer

---
+++ lua/layers/terraform.lua
@@ -0,0 +1,16 @@
+local layer = {}
+
+local plug = require("core.plug")
+
+function layer.register_plugins()
+	plug.add("hashivim/vim-terraform")
+end
+
+function layer.init_config()
+	local lsp = require("layers.lsp")
+	local nvim_lsp = require("nvim_lsp")
+
+	lsp.register_server(nvim_lsp.terraformls)
+end
+
+return layer
