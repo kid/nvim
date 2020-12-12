0f6e0b2a9704c7512cc2337865b5033caa0730da
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-12-12 18:17:52 +0100

    formatting

--- lua/layers/lua.lua
+++ lua/layers/lua.lua
@@ -4,23 +4,20 @@ function layer.init_config()
   local lsp = require("layers.lsp")
   local lspconfig = require("lspconfig")
 
-  lsp.register_server(
-  lspconfig.sumneko_lua,
-    {
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = {
-              "vim",
-            },
-          },
-          runtime = {
-            version = "LuaJIT",
+  lsp.register_server(lspconfig.sumneko_lua, {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = {
+            "vim",
           },
         },
+        runtime = {
+          version = "LuaJIT",
+        },
       },
-    }
-  )
+    },
+  })
 end
 
 return layer
