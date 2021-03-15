29871f7bebfb116637f62103174a2b6f25276bd4
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-03-15 08:43:35 +0100

    feat: add telescope layer

---
+++ lua/layers/telescope.lua
@@ -0,0 +1,15 @@
+local layer = {}
+
+function layer.plugins(use)
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {
+      'nvim-lua/popup.nvim',
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+    end
+  }
+end
+
+return layer
