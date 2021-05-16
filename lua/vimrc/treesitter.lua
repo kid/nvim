6edeef7505dd853d042fc975ef31d99d26b7930f
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-05-16 11:34:36 +0200

    treesitter: disable navigation and smart rename

--- lua/vimrc/treesitter.lua
+++ lua/vimrc/treesitter.lua
@@ -17,11 +17,11 @@ return function(use)
         refactor = {
           highlight_defintions = { enable = false },
           smart_rename = {
-            enable = true,
+            -- enable = true,
             -- smart_rename = 'grr',             -- mapping to rename reference under cursor
           },
           navigation = {
-            enable = true,
+            -- enable = true,
             -- goto_definition = 'gnd',          -- mapping to go to definition of symbol under cursor
             -- list_definitions = 'gnD'          -- mapping to list all definitions in current file
           },
