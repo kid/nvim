6ce009fb85a9cf780068b8523ffeeac1074e161c
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-06-29 17:52:04 +0200

    fix macos detection

--- lua/vimrc/orgmode.lua
+++ lua/vimrc/orgmode.lua
@@ -1,5 +1,5 @@
-function config()
-  local is_mac = vim.fn.has('macunix')
+function Config()
+  local is_mac = vim.fn.has('macunix') > 0
   local folder = is_mac and '/Volumes/org' or '~/org'
 
   require('orgmode').setup {
@@ -9,5 +9,5 @@ function config()
 end
 
 return function(use)
-  use { 'kristijanhusak/orgmode.nvim', config = config }
+  use { 'kristijanhusak/orgmode.nvim', config = Config }
 end
