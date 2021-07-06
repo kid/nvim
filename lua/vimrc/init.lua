a78ac00e4284c6fddde7d935a5db229374e87a70
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-07-06 12:24:46 +0200

    fix: indent-lien now using main branch

--- lua/vimrc/init.lua
+++ lua/vimrc/init.lua
@@ -140,7 +140,6 @@ require('packer').startup {
     }
     use {
       'lukas-reineke/indent-blankline.nvim',
-      branch = 'lua',
       config = function()
         vim.g.indent_blankline_char = '│'
         vim.g.indent_blankline_use_treesitter = true
