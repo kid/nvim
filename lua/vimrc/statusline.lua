dcfbdb73bf230e6ff6c6cdd12422b0253815bfc5
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-10-22 17:24:33 +0200

    use new lualine fork

--- lua/vimrc/statusline.lua
+++ lua/vimrc/statusline.lua
@@ -1,6 +1,6 @@
 return function(use)
   use {
-    'hoob3rt/lualine.nvim',
+    'nvim-lualine/lualine.nvim',
     requires = { { 'kyazdani42/nvim-web-devicons', opt = true } },
     config = function()
       require('lualine').setup { options = { section_separators = '', component_separators = '' } }
