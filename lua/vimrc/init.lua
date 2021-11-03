30946901b7e4bd1411286ed5bc59cc354a7b855a
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-11-03 13:52:34 +0100

    remove broken ,; bindings for lightspeed.nvim

--- lua/vimrc/init.lua
+++ lua/vimrc/init.lua
@@ -40,16 +40,6 @@ require('packer').startup {
           cycle_group_fwd_key = '<Tab',
           cycle_group_bwd_key = '<S-Tab>',
         }
-
-        function repeat_ft(reverse)
-          local ls = require 'lightspeed'
-          ls.ft['instant-repeat?'] = true
-          ls.ft:to(reverse, ls.ft['prev-t-like?'])
-        end
-        vim.api.nvim_set_keymap('n', ';', '<cmd>lua repeat_ft(false)<cr>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('x', ';', '<cmd>lua repeat_ft(false)<cr>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', ',', '<cmd>lua repeat_ft(true)<cr>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('x', ',', '<cmd>lua repeat_ft(true)<cr>', { noremap = true, silent = true })
       end,
     }
     use {
