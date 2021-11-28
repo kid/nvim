d9b3e2e1a050c97dd9b6d9afa8ee62758b45f6bb
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-11-28 12:12:31 +0100

    tweaks for man pages

--- lua/vimrc/init.lua
+++ lua/vimrc/init.lua
@@ -162,7 +162,7 @@ require('packer').startup {
       config = function()
         vim.g.indent_blankline_char = '│'
         vim.g.indent_blankline_use_treesitter = true
-        vim.g.indent_blankline_filetype_exclude = { 'NvimTree' }
+        vim.g.indent_blankline_filetype_exclude = { 'NvimTree', 'man' }
       end,
     }
     use {
