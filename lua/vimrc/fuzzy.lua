6dd93641c4766a46b3f75ceb6f092225027476f6
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-12-22 11:15:26 +0100

    use telescope-ui-select

--- lua/vimrc/fuzzy.lua
+++ lua/vimrc/fuzzy.lua
@@ -1,9 +1,7 @@
-
-
 return function(use)
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
+    requires = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-ui-select.nvim' },
     config = function()
       local wk = require('which-key')
       local builtin = require('telescope.builtin')
@@ -11,22 +9,25 @@ return function(use)
       local ensure_out_of_tree = function(func)
         return function()
           -- if NvimTree is currently focussed, switch to the previous window first
-          if vim.api.nvim_buf_get_option(0, 'ft') == 'NvimTree' then
-            vim.cmd('wincmd p')
-          end
+          if vim.api.nvim_buf_get_option(0, 'ft') == 'NvimTree' then vim.cmd('wincmd p') end
           func()
         end
       end
 
-      wk.register({
-        ["<leader>"] = {
-          ["<leader>"] = { ensure_out_of_tree(builtin.find_files), 'Find files' },
-          [','] = { ensure_out_of_tree(builtin.buffers), 'Buffers' },
-          h = { ensure_out_of_tree(builtin.help_tags), 'Help tags' },
-          s = { ensure_out_of_tree(builtin.live_grep), 'Search' },
-          k = { ensure_out_of_tree(builtin.keymaps), 'Keymaps' },
-        }
-      })
+      wk.register(
+        {
+          ['<leader>'] = {
+            ['<leader>'] = { ensure_out_of_tree(builtin.find_files), 'Find files' },
+            [','] = { ensure_out_of_tree(builtin.buffers), 'Buffers' },
+            h = { ensure_out_of_tree(builtin.help_tags), 'Help tags' },
+            s = { ensure_out_of_tree(builtin.live_grep), 'Search' },
+            k = { ensure_out_of_tree(builtin.keymaps), 'Keymaps' },
+          },
+        })
+
+      require('telescope').setup { extensions = { ['ui-select'] = { require('telescope.themes').get_dropdown {} } } }
+
+      require('telescope').load_extension('ui-select')
     end,
   }
 end
