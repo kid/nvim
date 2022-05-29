b0e4a2f0817e7dff61a3cfef3b2c8aaee6502216
Arnaud Rebts <arnaud.rebts@gmail.com>
2022-05-29 12:00:28 +0200

    enable lualine global status mode

--- lua/vimrc/statusline.lua
+++ lua/vimrc/statusline.lua
@@ -3,7 +3,7 @@ return function(use)
     'nvim-lualine/lualine.nvim',
     requires = { { 'kyazdani42/nvim-web-devicons', opt = true } },
     config = function()
-      require('lualine').setup { options = { section_separators = '', component_separators = '' } }
+      require('lualine').setup { options = { globalstatus = true, section_separators = '', component_separators = '' } }
     end,
   }
 end
