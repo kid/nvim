a8e48a6770334c965a0c437c3258661657e1a4ff
Arnaud Rebts <arnaud.rebts@gmail.com>
2022-06-12 08:36:53 +0200

    add gh.nvim

--- lua/vimrc/init.lua
+++ lua/vimrc/init.lua
@@ -55,7 +55,65 @@ require('packer').startup {
         require('marks').setup {}
       end,
     }
-    use { 'skanehira/gh.vim' }
+    use {
+      'ldelossa/gh.nvim',
+      requires = { 'ldelossa/litee.nvim' },
+      config = function()
+        require('litee.lib').setup()
+        require('litee.gh').setup()
+        local wk = require("which-key")
+        wk.register({
+          g = {
+            name = "+Git",
+            h = {
+              name = "+Github",
+              c = {
+                name = "+Commits",
+                c = { "<cmd>GHCloseCommit<cr>", "Close" },
+                e = { "<cmd>GHExpandCommit<cr>", "Expand" },
+                o = { "<cmd>GHOpenToCommit<cr>", "Open To" },
+                p = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
+                z = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
+              },
+              i = {
+                name = "+Issues",
+                p = { "<cmd>GHPreviewIssue<cr>", "Preview" },
+              },
+              l = {
+                name = "+Litee",
+                t = { "<cmd>LTPanel<cr>", "Toggle Panel" },
+              },
+              r = {
+                name = "+Review",
+                b = { "<cmd>GHStartReview<cr>", "Begin" },
+                c = { "<cmd>GHCloseReview<cr>", "Close" },
+                d = { "<cmd>GHDeleteReview<cr>", "Delete" },
+                e = { "<cmd>GHExpandReview<cr>", "Expand" },
+                s = { "<cmd>GHSubmitReview<cr>", "Submit" },
+                z = { "<cmd>GHCollapseReview<cr>", "Collapse" },
+              },
+              p = {
+                name = "+Pull Request",
+                c = { "<cmd>GHClosePR<cr>", "Close" },
+                d = { "<cmd>GHPRDetails<cr>", "Details" },
+                e = { "<cmd>GHExpandPR<cr>", "Expand" },
+                o = { "<cmd>GHOpenPR<cr>", "Open" },
+                p = { "<cmd>GHPopOutPR<cr>", "PopOut" },
+                r = { "<cmd>GHRefreshPR<cr>", "Refresh" },
+                t = { "<cmd>GHOpenToPR<cr>", "Open To" },
+                z = { "<cmd>GHCollapsePR<cr>", "Collapse" },
+              },
+              t = {
+                name = "+Threads",
+                c = { "<cmd>GHCreateThread<cr>", "Create" },
+                n = { "<cmd>GHNextThread<cr>", "Next" },
+                t = { "<cmd>GHToggleThread<cr>", "Toggle" },
+              },
+            },
+          },
+        }, { prefix = "<leader>" })
+      end
+    }
     use {
       'TimUntersberger/neogit',
       requires = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' },
@@ -63,7 +121,7 @@ require('packer').startup {
         local neogit = require('neogit')
         neogit.setup { kind = 'tab', disable_commit_confirmation = true, integrations = { diffview = true } }
 
-        require('which-key').register { ['<leader>g'] = { name = '+git', g = { neogit.open, 'Neogit' } } }
+        require('which-key').register { ['<leader>g'] = { name = '+Git', g = { neogit.open, 'Neogit' } } }
       end,
     }
     use {
