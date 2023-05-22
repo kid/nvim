d2b061bbb5d338b062225294f4694740b5118d39
Arnaud Rebts <arnaud.rebts@gmail.com>
2023-05-22 14:30:22 +0200

    fix(theme): Set CursorLineNr's background transparent
    We can't tweak the signcolumn's culhl, so make the cursorlinenr
    transparent to avoid a gap in the cusrorline

--- highlights/gruvbox.lua
+++ highlights/gruvbox.lua
@@ -3,11 +3,24 @@ local colors = require("gruvbox.palette").get_base_colors(vim.o.background, conf
 
 return {
   FoldColumn = { bg = nil },
-  SignColumn = { bg = nil },
+  -- SignColumn = { bg = nil },
   StatusLine = { fg = colors.fg1, bg = nil },
   -- StatusLine = { fg = colors.fg1, bg = colors.bg1 },
   -- GitSignsAdd = { bg = nil },
   -- GitSignsChange = { bg = nil },
   -- GitSignSDelete = { bg = nil },
   -- WinBarNC = { fg = colors.fg4, bg = nil },
+
+  -- https://github.com/ellisonleao/gruvbox.nvim/issues/230
+  SignColumn = { link = "Normal" },
+  GruvboxGreenSign = { bg = "" },
+  GruvboxOrangeSign = { bg = "" },
+  GruvboxPurpleSign = { bg = "" },
+  GruvboxYellowSign = { bg = "" },
+  GruvboxRedSign = { bg = "" },
+  GruvboxBlueSign = { bg = "" },
+  GruvboxAquaSign = { bg = "" },
+  CursorLineNr = { fg = colors.yellow, bg = nil },
+  -- CursorLineFold = { bg = colors.fg1 },
+  -- CursorLineSign = { bg = colors.fg1 },
 }
