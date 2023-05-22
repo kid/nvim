1db3c951be249d8aecccc86b9017b83b33c2cedb
Arnaud Rebts <arnaud.rebts@gmail.com>
2023-05-22 11:03:52 +0200

    fix(theme): tweak gruvbox config

--- highlights/gruvbox.lua
+++ highlights/gruvbox.lua
@@ -2,5 +2,12 @@ local config = require("gruvbox").config
 local colors = require("gruvbox.palette").get_base_colors(vim.o.background, config.contrast)
 
 return {
-  StatusLine = { fg = colors.fg1, bg = colors.bg1 }
+  FoldColumn = { bg = nil },
+  SignColumn = { bg = nil },
+  StatusLine = { fg = colors.fg1, bg = nil },
+  -- StatusLine = { fg = colors.fg1, bg = colors.bg1 },
+  -- GitSignsAdd = { bg = nil },
+  -- GitSignsChange = { bg = nil },
+  -- GitSignSDelete = { bg = nil },
+  -- WinBarNC = { fg = colors.fg4, bg = nil },
 }
